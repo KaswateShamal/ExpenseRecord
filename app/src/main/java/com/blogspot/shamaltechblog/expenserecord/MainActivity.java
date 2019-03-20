58511b96de5dcc4ac943b4ca23f70bf6ba9e16f2
KaswateShamal <shamal06project@gmail.com>
2019-03-20 18:19:05 +0530

    Yest

---
+++ app/src/main/java/com/blogspot/shamaltechblog/expenserecord/MainActivity.java
@@ -0,0 +1,15 @@
+package com.blogspot.shamaltechblog.expenserecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ///code goes here///
+    }
+}
+    
